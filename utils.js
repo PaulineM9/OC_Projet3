2795f31625da613e479081899b8bf71eb9077d33
PaulineM9 <paumichel32@gmail.com>
2019-02-19 10:29:18 +0100

    Code formating on utils.js

--- utils.js
+++ utils.js
@@ -3,15 +3,15 @@
 function ajaxGet(url, callback) {
 	var req = new XMLHttpRequest();
 	req.open("GET", url);
-	req.addEventListener("load", function() {
-		if (req.status >= 200 && req.status < 400){
+	req.addEventListener("load", function () {
+		if (req.status >= 200 && req.status < 400) {
 			callback(req.responseText); // ex: fonction callback = getStations(list_stations)
 			//console.log(req.responseText);
 		} else {
 			console.error(req.status + " " + req.statusText + " " + url);
 		}
 	});
-	req.addEventListener("error", function() {
+	req.addEventListener("error", function () {
 		console.error("Erreur réseau avec l'url " + url);
 	});
 	req.send(null);
@@ -31,7 +31,7 @@ function isStringValid(string) {
 	var reName = document.getElementById("perso_name").value;
 	var reFirstName = document.getElementById("perso_firstname").value;
 
-	if (string !== '' && typeof string == "string" && /[a-zA-Z]/.test(reName) && /[a-zA-Z]/.test(reFirstName) ) {
+	if (string !== '' && typeof string == "string" && /[a-zA-Z]/.test(reName) && /[a-zA-Z]/.test(reFirstName)) {
 		return true;
 	} else {
 		return false;
@@ -43,31 +43,32 @@ function timer() {
 		clearTimeout(self.timerId)
 	}
 	var timer_resa = document.getElementById("confirm_resa");
-	s=duree;
-	m=0;
-	h=0;
-	if(s<0) {
+	s = duree;
+	m = 0;
+	h = 0;
+	if (s < 0) {
 		timer_resa.innerHTML = "La durée de votre réservation a expiré." // remplacer par remove ('hidden') sur span html
 		self.reset_resa.style.display = "none";
 	} else {
-		if(s>59) {
-			m=Math.floor(s/60);
-			s=s-m*60;
+		if (s > 59) {
+			m = Math.floor(s / 60);
+			s = s - m * 60;
 		}
-		if(m>59) {
-			h=Math.floor(m/60);
-			m=m-h*60;
+		if (m > 59) {
+			h = Math.floor(m / 60);
+			m = m - h * 60;
 		}
-		if(s<10) {
-			s="0"+s;		
+		if (s < 10) {
+			s = "0" + s;
 		}
-		if(m<10) {
-			m="0"+m
+		if (m < 10) {
+			m = "0" + m
 		}
-			timer_resa.innerHTML = m+":"+s;
-		}
-		duree=duree-1;
-		self.timerId = window.setTimeout(timer,9);//indiquer 999
+		timer_resa.innerHTML = m + ":" + s;
 	}
+	duree = duree - 1;
+	self.timerId = window.setTimeout(timer, 9);//indiquer 999
+}
+
 
 
